5ca8bc6f2e93d0f71b0564e201139410a2490085
MosesKannan <moseskannan23.a@gmail.com>
2026-08-04 08:12:13 +0530

    Created login page UI

--- src/app/components/pages/login/login.ts
+++ src/app/components/pages/login/login.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  title = "PACB Login Portal";
+}
