4d3db1aef3f55479d93818a608fe85c0f2876569
moseslearn082-hub <moseslearn082@gmail.com>
2026-08-04 19:21:42 +0530

    Develop PACB login page and dashboard navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: '',
     component: Login
+  },
+    {
+    path: 'dashboard',
+    component: Dashboard
   }
 ];
